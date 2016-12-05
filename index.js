94914143cf6ee8a188b0de7ca95c986e47edd182
Sam Henderson <hendersonsam456@gmail.com>
2016-12-05 15:28:22 -0800

    heading swaps with each turn letting the players know who's turn it is

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// X goes first
 var player = 'X';
 
-    var aTags = document.getElementsByTagName("a");
-    for (var i=0;i<aTags.length;i++){
-        addEvent(aTags[i], 'click', alertWinner);
-    }
+// updateTurn changes the header to let you know whose turn it is
 
+var updateTurn = function() {
+  if (player === 'X') {
+    document.getElementById('turn').innerHTML = "It is Player 1's turn. They are the X's"
+  } else {
+    document.getElementById('turn').innerHTML = "It is Player 2's turn. They are the O's"
+  }
+}
 
+// add's the X or O when a grid element is clicked
 var tds = document.getElementsByTagName("td");
 
 for (var i = 0; i < tds.length; i++) {
@@ -13,10 +19,12 @@ for (var i = 0; i < tds.length; i++) {
     if (player === 'X' && this.innerHTML === '') {
       this.innerHTML = 'X'
       player = 'O'
+      updateTurn()
     }
     if (player === 'O' && this.innerHTML === '') {
       this.innerHTML = 'O'
       player = 'X'
+      updateTurn()
     }
   })
 }
